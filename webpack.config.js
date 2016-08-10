b20514fe6afcbefe8d92caeec5c1ec18edaf509a
yuyang041060120 <yuyang041060120@gmail.com>
2016-08-10 22:07:21 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/main',
+    vendor: './src/vendor',
+    polyfills: './src/polyfills'
+  },
+  output: {
+    path: './dist',
+    filename: '[name].bundle.js',
+    publicPath: '/dist/'
+  },
+  resolve: {
+    extensions: ['', '.js', '.ts']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'ts',
+        exclude: 'node_modules'
+      },
+      {
+        test: /\.html$/,
+        loader: 'raw',
+        exclude: './src/index.html'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['vendor', 'polyfills']
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
