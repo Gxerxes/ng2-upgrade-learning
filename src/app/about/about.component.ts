d65e0a5b38ecadc5b136abdcdf9dea9505736dee
yuyang041060120 <yuyang041060120@gmail.com>
2016-08-10 22:31:15 +0800

    add a component

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'about',
+    template: '<p>about</p>'
+})
+export class AboutComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
