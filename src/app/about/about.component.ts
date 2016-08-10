65a4c836ce9a126685b4bad7d51076bb8eabbdbe
yuyang041060120 <yuyang041060120@gmail.com>
2016-08-10 22:39:29 +0800

    add a pipe

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'about',
-    template: '<p highlight>about</p>'
+    template: '<p highlight>{{"about" | uppercase}}</p>'
 })
 export class AboutComponent implements OnInit {
     constructor() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { HighlightDirective } from './directives/highlight.directive';
+import { UppercasePipe } from './pipes/uppercase.pipe';
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent, AboutComponent, HighlightDirective],
+    declarations: [AppComponent, AboutComponent, HighlightDirective, UppercasePipe],
     bootstrap: [AppComponent]
 })
 export class AppModule {
